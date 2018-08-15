31a8c32dc448c02876e6ecb2b114714c56caed56
Joe Smith <yasumoto7@gmail.com>
2018-08-14 21:45:09 -0700

    How the heck did I merge this all wrong

--- Sources/AWSSDKSwiftCore/Signers/V4.swift
+++ Sources/AWSSDKSwiftCore/Signers/V4.swift
@@ -239,7 +239,7 @@ extension Signers {
             ].joined(separator: "\n")
         }
 
-        private static let awsUriAllowed: Characters = [
+        private static let awsUriAllowed: [String] = [
             "_", "-", "~", ".",
             "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
             "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
